ef9d65fad2b7b9e7aea153956d363803dd9eab3e
Himanshu Pal <136723213+abhiyanta-Himanshu@users.noreply.github.com>
2024-10-12 01:11:16 +0530

    Update constant.js for render link of backend

--- FrontEnd/src/utils/constant.js
+++ FrontEnd/src/utils/constant.js
@@ -1,9 +1,9 @@
-export const USER_API_END_POINT = "http://localhost:5050/api/user"
+export const USER_API_END_POINT = "https://counselconnect-backend.onrender.com//user"
 
-export const LAWYER_API_END_POINT = "http://localhost:5050/api/lawyer"
+export const LAWYER_API_END_POINT = "https://counselconnect-backend.onrender.com//lawyer"
 
-export const APPOINTMENT_API_END_POINT = "http://localhost:5050/api/appointments"
+export const APPOINTMENT_API_END_POINT = "https://counselconnect-backend.onrender.com/appointments"
 
-export const REVIEW_API_END_POINT = "http://localhost:5050/api/reviews"
+export const REVIEW_API_END_POINT = "https://counselconnect-backend.onrender.com//reviews"
 
-export const LEGAL_RESOURCES_API_END_POINT = "http://localhost:5050/api/legal-resources"
+export const LEGAL_RESOURCES_API_END_POINT = "https://counselconnect-backend.onrender.com//legal-resources"
