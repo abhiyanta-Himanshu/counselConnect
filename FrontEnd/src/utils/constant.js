902028ab92b7c857f930749a791eded10a5f73de
Himanshu Pal <136723213+abhiyanta-Himanshu@users.noreply.github.com>
2024-10-12 01:37:52 +0530

    Update constant.js troubleshoot the constant

--- FrontEnd/src/utils/constant.js
+++ FrontEnd/src/utils/constant.js
@@ -1,9 +1,9 @@
-export const USER_API_END_POINT = "https://counselconnect-backend.onrender.com//user"
+export const USER_API_END_POINT = "https://counselconnect-backend.onrender.com/api/user"
 
-export const LAWYER_API_END_POINT = "https://counselconnect-backend.onrender.com//lawyer"
+export const LAWYER_API_END_POINT = "https://counselconnect-backend.onrender.com/api/lawyer"
 
-export const APPOINTMENT_API_END_POINT = "https://counselconnect-backend.onrender.com/appointments"
+export const APPOINTMENT_API_END_POINT = "https://counselconnect-backend.onrender.com/api/appointments"
 
-export const REVIEW_API_END_POINT = "https://counselconnect-backend.onrender.com//reviews"
+export const REVIEW_API_END_POINT = "https://counselconnect-backend.onrender.com/api/api/reviews"
 
-export const LEGAL_RESOURCES_API_END_POINT = "https://counselconnect-backend.onrender.com//legal-resources"
+export const LEGAL_RESOURCES_API_END_POINT = "https://counselconnect-backend.onrender.com/api/legal-resources"
